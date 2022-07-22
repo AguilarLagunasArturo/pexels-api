20f0c1eb926605448cbacc47885858857aef9d5e
Manuel Alfonso Ortiz Lopez <podraco01@gmail.com>
2022-07-22 13:03:11 -0500

    Added Exceptions module.

---
+++ pexels_api/exceptions.py
@@ -0,0 +1,17 @@
+class PexelsInvalidToken(Exception):
+    pass
+
+class PexelsInvalidPhtoID(Exception):
+    pass
+
+class PexelsConnectionClosed(Exception):
+    pass
+
+class PexelsInvalidQuery(Exception):
+    pass
+
+class PexelsForbidden(Exception):
+    pass
+
+class PexelsResouceUnavailable(Exception):
+    pass
