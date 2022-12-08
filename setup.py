36774168a3e9f71bea7b136337b989ba4a93be60
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2022-12-08 16:00:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pexels-api-custom-voltaic",
-    version="1.0.2",
-    author="Voltaic314",
-    author_email="lmaupin@arizona.edu",
+    name="pexels-api",
+    version="1.1.0",
+    author="Arturo Aguilar Lagunas",
+    author_email="aguilar.lagunas.arturo@gmail.com",
     description="Use Pexels API v1 with Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Voltaic314/pexels-api-custom-voltaic",
+    url="https://github.com/AguilarLagunasArturo/pexels-api",
     keywords='pexels api images photos',
     install_requires=['requests'],
     packages=setuptools.find_packages(),
