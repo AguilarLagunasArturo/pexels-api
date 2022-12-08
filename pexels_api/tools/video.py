78ef83c31750c32194b601ec755da73ce2e8d845
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2022-12-08 15:56:09 -0500

    creating and updating video API support
    Trying to add video API support though frankly I'm not the best coder, but doing my best here.

--- pexels_api/tools/video.py
+++ pexels_api/tools/video.py
@@ -1,32 +1,56 @@
 # Author:           Voltaic314
 # Pexels Website:   https://www.pexels.com
-## This class is for the sole purpose of parsing json info from Pexels to get video links for nature poster script. 
+## This class is for the sole purpose of parsing json info from Pexels to get video links.
 ## Feel free to use this however you want, but I did cut out a lot of other things that you could use maybe if you have other purposes. 
 ## for the link on where I got this info, check out the pexels api docs here: https://www.pexels.com/api/documentation/ 
-## if the above link is dead, just search "pexels api documentation" and you'll probalby find it. 
-## if any of you have specific questions on how I did this, I encourage you to learn more about how objects and classes work in Python as well as JSON parsing. 
 
 class Video:
     def __init__(self, json_video):
         self.__video = json_video
+        
     @property
     def id(self):
         return int(self.__video["id"])
+    
     @property
     def width(self):
         return int(self.__video["width"])
+    
     @property
     def height(self):
         return int(self.__video["height"])
+    
     @property
     def videographer(self):
         return self.__video["user"]["name"]
+    
     @property
     def url(self):
         return self.__video["url"]
+    
     @property
     def description(self):
-        return self.url.split("/")[-2].replace("-{}".format(self.id), "")
+        return self.url.split("/")[-2].replace(f"-{self.id}", "").replace("-", " "))
+    
     @property
     def duration(self):
       return self.__video["duration"]
+
+    @property
+    def highest_resolution_video(self):
+        # Since each video will always have the same aspect ratio, just different resolutions....
+        # It really doesn't matter whether we sort by width or height here, we're just looking for the 
+        # highest number. 
+        return (max(l, key=lambda x: x['videos']['video_files']['width']))
+        
+    @property
+    def highest_resolution_width(self):
+        return int(self.highest_resolution_video['width'])
+    
+    @property
+    def highest_resolution_height(self):
+        return int(self.highest_resolution_video['height'])
+
+    @property
+    def link(self):
+        return self.highest_resolution_video['link']
